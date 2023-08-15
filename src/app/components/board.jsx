b07f84c754ace40ee48251de6468683ad19cb495
kruti-w <sanskruti290@hmail.com>
2023-08-14 22:47:43 -0700

    function to find the next empty cell

--- src/app/components/board.jsx
+++ src/app/components/board.jsx
@@ -32,3 +32,18 @@ export default function Board() {
     </div>
   )
 }
+
+function nextEmptyCell(cells, index){
+  for (let i = index; i < 81; i++){
+    if (cells[i] == ''){ // or do we write null???
+      return i;
+    }
+  }
+  for (let i = 0; i < index + 1; i++){
+    if (cells[i] == ''){ // or do we write null???
+      return i;
+    }
+  }
+  // no more empty cells
+  return null
+}
