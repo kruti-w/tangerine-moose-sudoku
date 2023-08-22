54ede510f970ff92552898d55e16b5d1e2c36c0f
kruti-w <sanskruti290@hmail.com>
2023-08-22 02:30:50 -0700

    Implemented hamburger icon

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,29 +1,45 @@
-export function Header({ title, appMode }) {
+import React from 'react';
+//import { FaBars, FaSearch } from 'react-icons/fa';
+
+// export function Header({ title, appMode }) {
+//   return (
+//     <header className="w-full flex-grow-0">
+//       <div className="flex justify-between uppercase">
+//         <div className="flex flex-col text-xs">
+//           <span>Sudoku</span>
+//           <span>Cruncher</span>
+//         </div>
+//         <div
+//           className="flex items-center text-sm select-none cursor-pointer"
+//           onClick={() => alert("This feature is coming soon")}
+//         >
+//           <div
+//             className={`p-2 border-r-2 border-black ${
+//               appMode === "creator" && "text-gray-400"
+//             }`}
+//           >
+//             Solver
+//           </div>
+//           <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>
+//             Creator
+//           </div>
+//         </div>
+//       </div>
+
+//       <h1 className="text-4xl mt-6 lg:text-center">{title}</h1>
+//     </header>
+//   );
+// }
+
+export function Header({ title, appMode }){
   return (
     <header className="w-full flex-grow-0">
-      <div className="flex justify-between uppercase">
-        <div className="flex flex-col text-xs">
-          <span>Sudoku</span>
-          <span>Cruncher</span>
-        </div>
-        <div
-          className="flex items-center text-sm select-none cursor-pointer"
-          onClick={() => alert("This feature is coming soon")}
-        >
-          <div
-            className={`p-2 border-r-2 border-black ${
-              appMode === "creator" && "text-gray-400"
-            }`}
-          >
-            Solver
-          </div>
-          <div className={`p-2 ${appMode === "solver" && "text-gray-400"}`}>
-            Creator
-          </div>
-        </div>
+      <div class="space-y-2">
+        <div class="w-7 h-0.5 bg-white"></div>
+        <div class="w-7 h-0.5 bg-white"></div>
+        <div class="w-7 h-0.5 bg-white"></div>
       </div>
 
-      <h1 className="text-4xl mt-6 lg:text-center">{title}</h1>
     </header>
   );
 }
